11f91b5b67b8f82423f0c19750c0af2a69d255e5
BushsHaxs <72326501+BushsHaxs@users.noreply.github.com>
2022-01-17 19:39:23 -0600

    pizzashowerman helped me

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,7 +4,6 @@ import flixel.tweens.misc.ColorTween;
 import flixel.math.FlxRandom;
 import openfl.net.FileFilter;
 import openfl.filters.BitmapFilter;
-import Shaders.PulseEffect;
 import Section.SwagSection;
 import Song.SwagSong;
 import flixel.FlxBasic;
